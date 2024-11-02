eb2d7a0d209181de709c1b40415adf388c2210dd
gnata <grahovanatalya@gmail.com>
2024-11-02 11:12:24 +0300

    Добавлена функция установки времени напоминания

--- Напоминать.py
+++ Напоминать.py
@@ -1,11 +1,26 @@
-from cProfile import label
 from tkinter import *
 from tkinter import messagebox as md
 from tkinter import  simpledialog as sd
 import datetime
 import time
 import  pygame
-from pygame import BUTTON_X1
+
+def set(mb=None):
+    rem = sd.askstring("Время напоминания", "Введите время напоминания в формате чч:мм" "(в 24 часовом формате)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка {e}")
+
+
 
 window = Tk()
 window.title("Напоминание")
